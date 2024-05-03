f0daab56a51166028c996f8b55220e599804b0de
Gabriel D'assumpção de Carvalho <92104540+gabrieldacarvalho@users.noreply.github.com>
2024-05-03 11:48:28 -0300

    Update cover_page.py

--- cover_page.py
+++ cover_page.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import requests
 
+# URL do arquivo README.md no repositório do GitHub
+raw_url = "https://raw.githubusercontent.com/gabrieldacarvalho/gabrieldacarvalho/main/README.md"
+
+# Fazendo a requisição para obter o conteúdo do arquivo
+response = requests.get(raw_url)
+
+
+st.image("https://www.example.com/sua_foto.jpg", caption='Sua Foto', use_column_width=True)
+
+st.markdown(response.text)
