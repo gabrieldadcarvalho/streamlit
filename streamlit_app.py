d59b79537d00e2ed60b95ca0a70267b6afc91310
Gabriel D'assumpção de Carvalho <92104540+gabrieldacarvalho@users.noreply.github.com>
2024-05-06 22:53:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,6 @@ def bebado():
     f = np.array([5, 5])
     historico = [i.copy()]
     recomecar = False
-
     if criterio == "Simular":
         q = st.slider("Qual o limite de passos:", 0, 1000, 0)
         if st.button("Aplicar"):
@@ -107,11 +106,20 @@ def bebado():
 
 
 def midsquare():
-    st.error("CÓDIGO AINDA ESTÁ EM CONSTRUÇÃO, APRESENTANDO ALGUMAS FALHAS")
-    lista_i = [st.number_input("Insira um número inicial de n dígitos:", 0, 9999, 0)]
-    lista_i_s = [str(lista_i[0])]
+    st.title("Método Mid-Square")
+    st.markdown("O método Mid-Square é uma técnica de geração de números aleatórios. Neste método, o primeiro número (semente/seed) com **n** dígitos é elevado ao quadrado. \
+                Em seguida, são utilizados os **n** dígitos centrais do resultado como o próximo número na sequência. No entanto, pode haver casos em que não conseguimos obter \
+                os **n** dígitos centrais do quadrado. Para ajustar isso, devemos incluir zeros à esquerda se necessário.")
+    st.warning("**Exemplo:**", icon="✏️")
+    st.write("x¹ = 124\
+             \n⮩ x¹ ** 2 = 1<u>**537**</u>6\
+             \nx² = 537\
+             \n⮩ x² ** 2 = 288369 ➟ 02<u>**883**</u>69 (correção)\
+             \nx³ = 883",unsafe_allow_html=True)
+    st.write('_'*3)
+    lista_i_s = [str(st.number_input("Insira um número inicial de n dígitos:", 0, 999999, 0))]
     q = int(st.number_input("Quantos números aleatórios você deseja: ", 1, 1000, 1))
-    if q != 0 and lista_i[0] > 1:
+    if q != 0 and int(lista_i_s[0]) > 0:
         for x in range(q):
             i_2 = int(lista_i_s[x]) ** 2
             i_2_s = [int(d) for d in str(i_2)]
@@ -120,17 +128,15 @@ def midsquare():
                 and int(lista_i_s[x]) % 2 == 0
                 or i_2 % 2 == 0
                 and int(lista_i_s[x]) % 2 != 0
-                or len(i_2_s) < len(lista_i_s[x])
+                or lista_i_s[x][0] == "0"
             ):
-
-                i_2 = ["0"] * (int(len(i_2_s) - len(lista_i_s[x]))) + i_2_s
+                i_2_s = ["0"] * (int(len(i_2_s) - len(lista_i_s[x]))) + i_2_s
                 media_i_1 = len(lista_i_s[x]) / 2
                 media_i_2 = len(i_2_s) / 2
                 sub = media_i_2 - media_i_1
                 i_2_s = i_2_s[int(sub) : int(len(i_2_s) - sub)]
                 lista_i_s.append("".join(str(d) for d in i_2_s))
             else:
-
                 media_i_1 = len(lista_i_s[x]) / 2
                 media_i_2 = len(i_2_s) / 2
                 sub = media_i_2 - media_i_1
