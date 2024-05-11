a932487265a58f8264c2aeb4c8c332959cc5455d
Gabriel D'assumpção de Carvalho <gabrieldadcarvalho@gmail.com>
2024-05-11 18:07:23 -0300

    Update def_modelagem_simulacao.py

--- def_modelagem_simulacao.py
+++ def_modelagem_simulacao.py
@@ -1,22 +1,19 @@
 import streamlit as st
-import requests
 import numpy as np
 import plotly.graph_objects as go
 import pandas as pd
 
+
 def bebado():
     def chega_5_5(i, f, direcao, historico):
-        cont = 0
-        while not np.all(i == f):
-            direcao_escolhida = np.random.choice(range(len(direcao)))
-            i += direcao[direcao_escolhida]
+        for _ in range(2000):
+            i += direcao[np.random.choice(len(direcao))]
             historico.append(i.copy())
-            cont += 1
-            if cont == 2000:
-                st.error("O BÊBADO SE PERDEU DEPOIS DE 2000 PASOS")
-                st.write("Se quiser recomeçar, aperte em aplicar novamente")
+            if np.all(i == f):
+                st.success("O BÊBADO CHEGOU NO PONTO (5,5)")
                 return True
-        st.success("O BÊBADO CHEGOU NO PONTO (5,5)")
+        st.error("O BÊBADO SE PERDEU DEPOIS DE 2000 PASOS")
+        st.write("Se quiser recomeçar, aperte em aplicar novamente")
         return False
 
     st.title("Andar do Bêbado")
@@ -33,36 +30,40 @@ def bebado():
                 Ao selecionar "Chegar no ponto (5,5)", o bêbado tentará mover-se para o ponto (5,5).\
                     Se não chegar em no máximo 2000 passos, o programa é encerrado.'
     )
-    st.warning("**GRANDES QUANTIDADES DE SIMULAÇÕES PODEM DEIXAR A PÁGINA MAIS LENTA** ", icon="⚠️")
+    st.warning(
+        "**GRANDES QUANTIDADES DE SIMULAÇÕES PODEM DEIXAR A PÁGINA MAIS LENTA** ",
+        icon="⚠️",
+    )
 
     criterio = st.radio(
-    "Selecione uma opção",
-    ("Simular", "Chegar no ponto (5,5)"),
-    format_func=lambda x: "Simular" if x == "Simular" else "Chegar no ponto (5,5)",)
+        "Selecione uma opção",
+        ("Simular", "Chegar no ponto (5,5)"),
+        format_func=lambda x: "Simular" if x == "Simular" else "Chegar no ponto (5,5)",
+    )
     direcao = [(0, 1), (0, -1), (1, 0), (-1, 0)]  # (norte, sul, leste, oeste)
     i = np.array([0, 0])
     f = np.array([5, 5])
     historico = [i.copy()]
     recomecar = False
+
     if criterio == "Simular":
         q = st.slider("Qual o limite de passos:", 0, 5000, 0)
         if st.button("Aplicar"):
-            if q != 0:
-                for passo in range(1, q + 1):
-                    direcao_escolhida = np.random.choice(range(len(direcao)))
-                    i += direcao[direcao_escolhida]
-                    historico.append(i.copy())
-                    if np.all(i == f):
-                        st.success(
-                            "O BÊBADO CHEGOU NO PONTO (5,5) depois de "
-                            + str(len(historico) - 1)
-                            + " pasos"
-                        )
-                        break
-                    elif passo == q and not np.all(i == f):
-                        st.error("O BÊBADO SE PERDEU DEPOIS DE " + str(q) + " PASOS")
-                        st.write("Se quiser recomeçar, aperte em aplicar novamente")
-                        recomecar = True
+            for passo in range(1, q + 1):
+                i += direcao[np.random.choice(len(direcao))]
+                historico.append(i.copy())
+                if np.all(i == f):
+                    st.success(
+                        "O BÊBADO CHEGOU NO PONTO (5,5) depois de "
+                        + str(len(historico) - 1)
+                        + " pasos"
+                    )
+                    break
+            else:
+                if not np.all(i == f):
+                    st.error("O BÊBADO SE PERDEU DEPOIS DE " + str(q) + " PASOS")
+                    st.write("Se quiser recomeçar, aperte em aplicar novamente")
+                    recomecar = True
 
     elif criterio == "Chegar no ponto (5,5)":
         if st.button("Aplicar"):
@@ -71,77 +72,115 @@ def bebado():
     if len(historico) > 1 and not recomecar:
         st.write("Gráfico de passos: ")
         fig = go.Figure()
-        
-        for i in range(len(historico) - 1):
-            fig.add_trace(go.Scatter(
-                x=[historico[i][0], historico[i + 1][0]],
-                y=[historico[i][1], historico[i + 1][1]],
+
+        # Otimizando a criação do gráfico
+        x_coords, y_coords = zip(*historico)
+        fig.add_trace(
+            go.Scatter(
+                x=x_coords,
+                y=y_coords,
                 mode="lines",
                 line=dict(color="#ADD8E6", width=2),
-                name='Passo ' + str(i + 1),
-            ))
+                name="Passos",
+            )
+        )
 
-        fig.add_trace(go.Scatter(
-            x=[historico[0][0]],
-            y=[historico[0][1]],
-            mode="markers",
-            marker=dict(color="green", size=10),
-            name="Partida",
-        ))
+        fig.add_trace(
+            go.Scatter(
+                x=[historico[0][0]],
+                y=[historico[0][1]],
+                mode="markers",
+                marker=dict(color="green", size=10),
+                name="Partida",
+            )
+        )
 
-        fig.add_trace(go.Scatter(
-            x=[historico[-1][0]],
-            y=[historico[-1][1]],
-            mode="markers",
-            marker=dict(color="red", size=10),
-            name="Chegada",
-        ))
+        fig.add_trace(
+            go.Scatter(
+                x=[historico[-1][0]],
+                y=[historico[-1][1]],
+                mode="markers",
+                marker=dict(color="red", size=10),
+                name="Chegada",
+            )
+        )
 
         st.plotly_chart(fig)
 
 
 def midsquare():
     st.title("Método Mid-Square")
-    st.markdown("O método Mid-Square é uma técnica de geração de números aleatórios. Neste método, o primeiro número (semente/seed) com **n** dígitos é elevado ao quadrado. \
+    st.markdown(
+        "O método Mid-Square é uma técnica de geração de números aleatórios. Neste método, o primeiro número (semente/seed) com **n** dígitos é elevado ao quadrado. \
                 Em seguida, são utilizados os **n** dígitos centrais do resultado como o próximo número na sequência. No entanto, pode haver casos em que não conseguimos obter \
-                os **n** dígitos centrais do quadrado. Para ajustar isso, devemos incluir zeros à esquerda se necessário.")
+                os **n** dígitos centrais do quadrado. Para ajustar isso, devemos incluir zeros à esquerda se necessário."
+    )
     st.warning("**Exemplo:**", icon="✏️")
-    st.write("x¹ = 124\
+    st.write(
+        "x¹ = 124\
              \n⮩ x¹ ** 2 = 1<u>**537**</u>6\
              \nx² = 537\
              \n⮩ x² ** 2 = 288369 ➟ 02<u>**883**</u>69 (correção)\
-             \nx³ = 883",unsafe_allow_html=True)
-    st.write('_'*3)
-    st.session_state.lista_n = [str(st.number_input("Insira um número inicial de n dígitos:", 0, 99999, 0))]
+             \nx³ = 883",
+        unsafe_allow_html=True,
+    )
+    st.write("_" * 3)
+
+    # Usando st.session_state para armazenar os dados da sessão
+    if "lista_n" not in st.session_state:
+        st.session_state.lista_n = []
+
+    st.session_state.lista_n.append(
+        str(st.number_input("Insira um número inicial de n dígitos:", 0, 99999, 0))
+    )
     q = int(st.number_input("Quantos números aleatórios você deseja: ", 1, 1000, 1))
     padronizar = st.radio(
-    "Você quer padronizar os números gerados?",
-    ("Sim", "Não"),
-    format_func=lambda x: "Sim" if x == "Sim" else "Não",)
+        "Você quer padronizar os números gerados?",
+        ("Sim", "Não"),
+        format_func=lambda x: "Sim" if x == "Sim" else "Não",
+    )
+
     if st.button("Gerar Números"):
         for x in range(q):
             x_2 = str(int(st.session_state.lista_n[x]) ** 2)
-            if st.session_state.lista_n[x][0] == '0':
+            if st.session_state.lista_n[x][0] == "0":
                 x_2 = x_2.zfill(len(x_2) + 1)
             n = len(str(st.session_state.lista_n[x]))
             n2 = len(x_2)
-            mid_x_2 = x_2[(n2 - n) // 2 :(n2 + n) // 2]
-            st.session_state.lista_n.append(mid_x_2) # Converter para int
-        st.session_state.df_n = pd.DataFrame(st.session_state.lista_n, columns=["Nº Gerados"])
+            mid_x_2 = x_2[(n2 - n) // 2 : (n2 + n) // 2]
+            st.session_state.lista_n.append(mid_x_2)
+        st.session_state.df_n = pd.DataFrame(
+            st.session_state.lista_n, columns=["Nº Gerados"]
+        )
+
+        # Criando colunas para a exibição dos dados
         col1, col2 = st.columns([1, 3])
         with col1:
-         st.dataframe(st.session_state.df_n)
+            st.dataframe(st.session_state.df_n)
         with col2:
             if padronizar == "Sim":
-                df_n_p_list = []
-                for i in range(len(st.session_state.df_n)):
-                    num = int(st.session_state.df_n["Nº Gerados"][i])
-                    padronizado = num / 10**len(str(num))
-                    df_n_p_list.append(padronizado)
-                st.session_state.df_n_p = pd.DataFrame(df_n_p_list, columns=["Nº Padronizados"])
+                df_n_p_list = [
+                    int(num) / 10 ** len(str(num))
+                    for num in st.session_state.df_n["Nº Gerados"]
+                ]
+                st.session_state.df_n_p = pd.DataFrame(
+                    df_n_p_list, columns=["Nº Padronizados"]
+                )
                 st.dataframe(st.session_state.df_n_p)
                 with col1:
-                    st.download_button(label='Baixar Dados', data=pd.concat([st.session_state.df_n, st.session_state.df_n_p], axis=1).to_csv(index=False), file_name='mid_square.csv', mime='text/csv')
+                    st.download_button(
+                        label="Baixar Dados",
+                        data=pd.concat(
+                            [st.session_state.df_n, st.session_state.df_n_p], axis=1
+                        ).to_csv(index=False),
+                        file_name="mid_square.csv",
+                        mime="text/csv",
+                    )
             else:
                 with col1:
-                 st.download_button(label='Baixar Dados', data=st.session_state.df_n.to_csv(index=False), file_name='mid_square.csv', mime='text/csv')
+                    st.download_button(
+                        label="Baixar Dados",
+                        data=st.session_state.df_n.to_csv(index=False),
+                        file_name="mid_square.csv",
+                        mime="text/csv",
+                    )
