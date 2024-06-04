a581671133df88dd1ea05448c02bd2d6ed7999d4
Gabriel D'assumpção de Carvalho <gabrieldadcarvalho@gmail.com>
2024-06-04 16:55:21 -0300

    Update def_modelagem_simulacao.py

--- def_modelagem_simulacao.py
+++ def_modelagem_simulacao.py
@@ -104,18 +104,6 @@ def bebado():
         )
         st.plotly_chart(fig)
 
-    # if len(historico) > 1:
-    #     # Adiciona o botão de download do arquivo CSV
-    #     x_coords, y_coords = zip(*historico)
-    #     historico_df = pd.DataFrame({"x": x_coords, "y": y_coords})
-    #     csv = historico_df.to_csv(index=False)
-    #     st.download_button(
-    #         label="Baixar histórico como CSV",
-    #         data=csv,
-    #         file_name="historico_bebado.csv",
-    #         mime="text/csv",
-    #     )
-
 
 def midsquare():
     st.title("Método Mid-Square")
@@ -209,6 +197,7 @@ def congruencia():
         \n* **X_n** é o número atual na sequência.  \
         \n* **a** é o multiplicador.  \
         \n* **c** é o incremento.  \
+        \n **mod** é a abreviação de módulo, que é o resto de um divisão.\
         \n* **m** é o módulo.  \
         \n \
         \nA escolha adequada dos parâmetros a, c e m é crucial para garantir a qualidade dos números pseudo-aleatórios gerados.",
@@ -217,11 +206,16 @@ def congruencia():
     st.warning("**Exemplo:**", icon="✏️")
     st.write(
         "Vamos considerar os seguintes parâmetros: a = 2, c = 1, m = 10.\
-        \nSe o valor inicial (semente) for X_0 = 3, os próximos números da sequência seriam:\
+        \nSe o valor inicial (semente) for X_0 = 3, os próximos números da sequência seriam: \
         \n* X_1 = (2 * 3 + 1) mod 10 = 7  \
         \n* X_2 = (2 * 7 + 1) mod 10 = 5  \
         \n* X_3 = (2 * 5 + 1) mod 10 = 1  \
-        \n* X_4 = (2 * 1 + 1) mod 10 = 3\
+        \n* X_4 = (2 * 1 + 1) mod 10 = 3  \
+        \n* X_5 = (2 * 3 + 1) mod 10 = 7  \
+        \n* X_6 = (2 * 7 + 1) mod 10 = 5  \
+        \n* X_7 = (2 * 5 + 1) mod 10 = 1  \
+        \n* X_8 = (2 * 1 + 1) mod 10 = 3  \
+        \n*... \
         \n \
         \nObserve que a sequência começa a se repetir após X_4. A qualidade do gerador LCG depende fortemente da escolha dos parâmetros."
     )
@@ -229,13 +223,13 @@ def congruencia():
     # Obter parâmetros do usuário
     modulo = st.number_input("Insira o módulo (0 < m)", 1, 99999, 1)
     multiplicador = st.number_input(
-        "Insira o multiplicador (0 < a < m):", 0, (modulo - 1), 0
+        "Insira o multiplicador (0 < a < m):", 0.00000001, (modulo - 0.00000001), 0
     )
     incremento = st.number_input(
-        "Insira o incremento (0 \u2264 c < m):", 0, (modulo - 1), 0
+        "Insira o incremento (0 \u2264 c < m):", 0, (modulo - 0.00000001), 0
     )
     numero_inicial = st.number_input(
-        "Insira um número inicial (0 \u2264 x0 < m):", 0, (modulo - 1), 0
+        "Insira um número inicial (0 \u2264 x0 < m):", 0, (modulo - 0.00000001), 0
     )
     quantidade_numeros = st.number_input(
         "Quantos números você quer gerar? ", 1, 999999, 1
@@ -246,15 +240,13 @@ def congruencia():
         if modulo <= 0:
             st.error("O módulo deve ser maior que 0.")
         elif not (0 < multiplicador < modulo):
-            st.error("O multiplicador deve estar entre 0 e m.")
+            st.error("O multiplicador deve estar entre (0,m).")
         elif not (0 <= incremento < modulo):
-            st.error("O incremento deve estar entre 0 e m (inclusive).")
+            st.error("O incremento deve estar entre [0,m).")
         elif not (0 <= numero_inicial < modulo):
-            st.error("O número inicial deve estar entre 0 e m-1.")
+            st.error("O número inicial deve estar entre [0,m).")
         elif quantidade_numeros <= 0:
-            st.error(
-                "A quantidade de números a serem gerados deve ser um valor positivo."
-            )
+            st.error("A quantidade de números a serem gerados deve ser [1,999999].")
         else:
             # Gerar números pseudo-aleatórios usando o LCG
             numeros_gerados = np.zeros(quantidade_numeros + 1, dtype=int)
@@ -275,8 +267,7 @@ def congruencia():
 
             if padronizar == "Sim":
                 numeros_padronizados = [
-                    int(num) / int(modulo)
-                    for num in dataframe_numeros_gerados["Números Gerados"]
+                    num / modulo for num in dataframe_numeros_gerados["Números Gerados"]
                 ]
                 dataframe_numeros_padronizados = pd.DataFrame(
                     numeros_padronizados, columns=["Números Padronizados"]
