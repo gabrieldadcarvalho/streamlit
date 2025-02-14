d854fd715586210e03f2b6cba5a4fc04fd3e9193
gabrieldadcarvalho <gabrieldadcarvalho@gmail.com>
2025-02-14 16:59:43 -0300

    fix: error show pdf ibovespa distribution

--- modeling_simulation/ibovespa_distribution/page_ibovespa.py
+++ modeling_simulation/ibovespa_distribution/page_ibovespa.py
@@ -1,4 +1,30 @@
 import streamlit as st
+import base64
+import requests
+import tempfile
+
+
+def displayPDF(url):
+    # Baixar o PDF da URL
+    response = requests.get(url)
+    if response.status_code == 200:
+        # Criar um arquivo temporário para armazenar o PDF
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_pdf:
+            tmp_pdf.write(response.content)
+            pdf_path = tmp_pdf.name
+
+        # Ler e converter para base64
+        with open(pdf_path, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode("utf-8")
+
+        # Gerar HTML para embutir o PDF
+        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+
+        # Mostrar o PDF no Streamlit
+        st.markdown(pdf_display, unsafe_allow_html=True)
+    else:
+        st.error("Erro ao baixar o PDF. Verifique a URL.")
+
 
 def show_data_simulation_pdf():
     st.title("Análise Estatística dos Retornos Diários do Índice Ibovespa")
@@ -21,11 +47,9 @@ def show_data_simulation_pdf():
         do IBOV e seu comportamento estatístico.
         """
         )
-
+    
     pdf_url = "https://nbviewer.org/github/gabrieldadcarvalho/modeling_simulatiton/blob/main/works/ibov_index/data_simulation.pdf"
 
-    pdf_viewer = f"""
-        <embed src="{pdf_url}" width="800" height="600" type="application/pdf">
-    """
-    st.markdown(pdf_viewer, unsafe_allow_html=True)
-    #st.components.v1.html(pdf_viewer, height=700)
+    # Exibir o PDF
+    displayPDF(pdf_url)
+
