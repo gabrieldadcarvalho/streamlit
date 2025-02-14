2c8db6753f2ea5af34196838d6ca576a8af4ecd8
gabrieldadcarvalho <gabrieldadcarvalho@gmail.com>
2025-02-14 17:47:13 -0300

    fix displayPDF

--- ai/health_insurance.py
+++ ai/health_insurance.py
@@ -1,17 +1,15 @@
 import streamlit as st
 import base64
 import requests
-import tempfile
-from streamlit_pdf_viewer import pdf_viewer
+
 
 def displayPDF(url):
-
-    # Fazendo o download do PDF
     response = requests.get(url)
 
     if response.status_code == 200:
-        # Exibe o PDF utilizando o conteúdo binário
-        pdf_viewer(input=response.content, width=700)
+        base64_pdf = base64.b64encode(response.content).decode("utf-8")
+        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+        st.markdown(pdf_display, unsafe_allow_html=True)
     else:
         st.error(f"Erro ao carregar o PDF. Status: {response.status_code}")
 
