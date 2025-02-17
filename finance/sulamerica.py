63f6a23c7d76bf3aeb9df3a7635cfa908f1e0741
gabrieldadcarvalho <gabrieldadcarvalho@gmail.com>
2025-02-17 18:17:59 -0300

    sulamerica

--- finance/sulamerica.py
+++ finance/sulamerica.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from functions.displaypdf import displayPDFile
+from functions.displaypdf import displayPDF
 
 
 def sulamericaPDF():
@@ -13,17 +13,18 @@ def sulamericaPDF():
 
     st.markdown(
         """
-    O relatório a seguir foi elaborado e publicado publicamente durante minha atuação como **Analista Macroeconômico** na **Liga de Mercado Financeiro da UFPB**. Ele apresenta uma análise detalhada da **SulAmérica Seguros**, baseada em dados financeiros e econômicos disponíveis no mercado.
+    O relatório a seguir foi elaborado e publicado publicamente durante minha atuação como **Analista Macroeconômico** na **Liga de Mercado Financeiro da UFPB** (LMF-UFPB). Ele apresenta uma análise detalhada da **SulAmérica Seguros**, baseada em dados financeiros e econômicos disponíveis no mercado.
 
     O estudo inclui uma avaliação tanto do setor quanto da empresa, culminando na apresentação de um **valuation**, desenvolvido em conjunto por todos os integrantes do grupo.
 
     ### **Minha contribuição**
     Minha principal atuação no projeto foi na **análise setorial e no estudo da atuação da empresa no mercado**, fornecendo insights estratégicos para a avaliação do negócio.
 
+    link da LMF-UFPB: https://lmfufpb.wixsite.com/lmfufpb
+
     """
     )
-
-    pdf = "streamlit/finance/pdf/sulamerica.pdf"
+    pdf_url = "https://github.com/gabrieldadcarvalho/streamlit/raw/main/finance/pdf/sulamerica.pdf"
 
     # Exibir o PDF e botão de download
-    displayPDFile(pdf, "sulamerica")
+    displayPDF(pdf_url, "business_case_sulamerica")
